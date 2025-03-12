c9ce298fd9c9159d787207b64203b53cc1ef040a
DigitalCRX <mdonahue09@gmail.com>
2025-03-12 19:17:08 -0400

    Update main.tf
    Making the code cleaner and adding variables

--- main.tf
+++ main.tf
@@ -1,43 +1,23 @@
-#provider "aws" {
-#  profile = "default"
-#  region  = "us-east-1"
-#}
-#
 data "aws_vpc" "default" {
   default = true
 }
 
 resource "aws_security_group" "aws_hello_sg" {
-  name        = "http_in"
-  description = "Allow HTTP & HTTPS traffic in port 8888 and everything out"
+  name        = "aws_hello_sg"
+  description = "Allow HTTP & HTTPS traffic in port 8888"
+  vpc_id      = data.aws_vpc.default.id
 
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "aws_hello_http_in" {
-  type        = "ingress"
-  from_port   = 8888
-  to_port     = 8888
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.aws_hello_sg.id
-}
-
-resource "aws_security_group_rule" "aws_hello_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.aws_hello_sg.id
+  ingress {
+    from_port   = var.in_port
+    to_port     = var.in_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "hello_ec2" {
-  ami           = "ami-0c7af5fe939f2677f"
-  instance_type = "t2.micro"
-
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.aws_hello_sg.id]
 
   user_data = <<-EOF
@@ -45,7 +25,7 @@ resource "aws_instance" "hello_ec2" {
     yum -y install httpd
     echo "<h1>Hello world</h1>" > /var/www/html/index/html
     sudo systemctl enable httpd
-    sudo ayarwmctl start httpd
+    sudo systemctl start httpd
     EOF
 
   tags = {
