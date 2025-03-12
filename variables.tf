7745c4380916014c3a0d7beab6fde0ac64887789
DigitalCRX <mdonahue09@gmail.com>
2025-03-12 18:49:51 -0400

    Update variables.tf
    Updating initial variable values

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "instance_ami" {
   description = "AMI ID for the EC2 instance"
   type = string
-  default = ""
+  default = "ami-0c7af5fe939f2677f"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
   type = string
-  default = ""
+  default = "t2.micro"
 }
