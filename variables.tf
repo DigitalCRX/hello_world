45c082be9cf5ffd1b3ed97a6f1e2aed116ad9c3c
DigitalCRX <mdonahue09@gmail.com>
2025-03-13 15:40:55 -0400

    Update variables.tf
    Formatting changes

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "instance_ami" {
   description = "AMI ID for the EC2 instance"
-  type = string
-  default = "ami-0c7af5fe939f2677f"
+  type        = string
+  default     = "ami-0c7af5fe939f2677f"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "in_port" {
   description = "Ingress port number for security group tied to EC2 hello instance"
-  type = number
-  default = 80
+  type        = number
+  default     = 80
 }
